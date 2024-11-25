823e5819c8a3b01c98f16a5636329e5739d0f4e9
EimerLegui <eimerlegui98@gmail.com>
2024-11-24 20:54:19 -0500

    refactor: :recycle: update HttpAbstract

--- front/src/abstracts/http/Http.abstract.ts
+++ front/src/abstracts/http/Http.abstract.ts
@@ -1,19 +1,19 @@
-import { IHttp, IOptionsHttp } from '@/types';
-import { AxiosInstance } from 'axios';
+import { IHttp } from '@/types';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 export abstract class Http implements IHttp {
 	protected constructor(private axiosInstance: AxiosInstance) { }
 
-	async get<T>(url: string, params?: any, options?: IOptionsHttp): Promise<T> {
+	async get<T>(url: string, params?: any, options?: AxiosRequestConfig): Promise<T> {
 		return await this.axiosInstance.get(url, { params, ...options }) as T
 	}
-	async post<T>(url: string, params?: any, options?: IOptionsHttp): Promise<T> {
+	async post<T>(url: string, params?: any, options?: AxiosRequestConfig): Promise<T> {
 		return await this.axiosInstance.post(url, params, { ...options }) as T
 	}
-	async put<T>(url: string, params?: any, options?: IOptionsHttp): Promise<T> {
+	async put<T>(url: string, params?: any, options?: AxiosRequestConfig): Promise<T> {
 		return await this.axiosInstance.put(url, params, { ...options }) as T
 	}
-	async delete<T>(url: string, params?: any, options?: IOptionsHttp): Promise<T> {
+	async delete<T>(url: string, params?: any, options?: AxiosRequestConfig): Promise<T> {
 		return await this.axiosInstance.delete(url, { params, ...options }) as T
 	}
 }
